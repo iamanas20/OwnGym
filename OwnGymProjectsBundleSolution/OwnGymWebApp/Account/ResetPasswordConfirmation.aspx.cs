5bea24dbdecfafcabccb69422a37298771eea334
iamanas20 <annouslatique@live.fr>
2018-04-18 10:22:21 +0100

    added new project and 80% on the header

---
+++ OwnGymProjectsBundleSolution/OwnGymWebApp/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace OwnGymWebApp.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ OwnGymProjectsBundleSolution/OwnGymWebApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OwnGymWebApp.Global" Language="C#" %>
